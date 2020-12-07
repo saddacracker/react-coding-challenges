ffb33c3850777f912159fb0b9991f64613b87ea8
Johnny Cardwell <cardwell.john@gmail.com>
2020-12-07 12:41:51 -0800

    remove commenting

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -7,10 +7,6 @@ export default function LaunchPad() {
 
   setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
 
-  console.log('====================================');
-  console.log('rerenderCount', rerenderCount);
-  console.log('====================================');
-
   return (
     <div className="launchpad">
       <ClassRocket />
